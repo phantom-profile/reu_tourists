83d0670bb00597bd077064d88acafcba9e4eb0b9
Andrew Rumyantsev <a.rumyantsev@sftpro.ru>
2021-12-02 18:11:30 +0300

    connected bootstrap to project

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+require('jquery')
+import "bootstrap"
+import "@popperjs/core"
+import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
