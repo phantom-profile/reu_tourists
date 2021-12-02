fe1d18188f3fd42b16e717b3193312b44eadf2a1
Andrew Rumyantsev <a.rumyantsev@sftpro.ru>
2021-12-02 18:14:28 +0300

    added User model

---
+++ db/migrate/20211202133433_add_is_elder_to_user.rb
@@ -0,0 +1,5 @@
+class AddIsElderToUser < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :is_elder, :boolean, null: false, default: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_01_195350) do
+ActiveRecord::Schema.define(version: 2021_12_02_133433) do
 
   create_table "schools", force: :cascade do |t|
     t.string "name", null: false
@@ -37,6 +37,21 @@ ActiveRecord::Schema.define(version: 2021_12_01_195350) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["sword_type_id"], name: "index_swords_on_sword_type_id"
     t.index ["witcher_id"], name: "index_swords_on_witcher_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name", null: false
+    t.string "nickname"
+    t.boolean "is_elder", default: false, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "witchers", force: :cascade do |t|
