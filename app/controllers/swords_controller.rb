9e397fcc83558e2764b2f2ad80bec91ecd5d1d7c
Andrew Rumyantsev <a.rumyantsev@sftpro.ru>
2021-12-06 13:50:52 +0300

    add one-page editing of sword resource

--- app/controllers/swords_controller.rb
+++ app/controllers/swords_controller.rb
@@ -1,23 +1,37 @@
 class SwordsController < ApplicationController
   before_action :is_elder?, only: [:create, :destroy]
+  before_action :sword, only: [:edit, :update, :destroy]
+
+  def initialize
+    super
+    @new_sword = Sword.new
+  end
 
   def index
     @swords = Sword.all.preload(:witcher, :sword_type)
-    @sword = Sword.new
-  end
-
-  def destroy
-    @sword = Sword.find(params[:id])
-    @sword.destroy
   end
 
   def create
     @sword = Sword.create(sword_params)
   end
 
+  def edit; end
+
+  def update
+    @sword.update(sword_params)
+  end
+
+  def destroy
+    @sword.destroy
+  end
+
   private
 
   def sword_params
     params.require(:sword).permit(:name, :sword_type_id, :min_damage, :max_damage)
+  end
+
+  def sword
+    @sword = Sword.find(params[:id])
   end
 end
