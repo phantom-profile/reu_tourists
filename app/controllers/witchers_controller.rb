5c4f1be363c7b48652aa777d30d4532b0587c2e6
Andrew Rumyantsev <a.rumyantsev@sftpro.ru>
2021-12-06 14:04:37 +0300

    add one-page editing for witcher resource

--- app/controllers/witchers_controller.rb
+++ app/controllers/witchers_controller.rb
@@ -1,18 +1,28 @@
 class WitchersController < ApplicationController
   before_action :is_elder?, only: [:create, :destroy]
+  before_action :witcher, only: [:edit, :update, :destroy]
+
+  def initialize
+    super
+    @new_witcher = Witcher.new
+  end
 
   def index
     @witchers = Witcher.all
-    @witcher = Witcher.new
-  end
-
-  def destroy
-    @witcher = Witcher.find(params[:id])
-    @witcher.destroy
   end
 
   def create
     @witcher = Witcher.create(witcher_params)
+  end
+
+  def edit; end
+
+  def update
+    @witcher.update(witcher_params)
+  end
+
+  def destroy
+    @witcher.destroy
   end
 
   private
@@ -21,5 +31,9 @@ class WitchersController < ApplicationController
     cleaned_params = params.require(:witcher).permit(:name, :nickname, :age, :sword, :location, :school_id)
     cleaned_params[:sword] = Sword.find(cleaned_params[:sword])
     cleaned_params
+  end
+
+  def witcher
+    @witcher = Witcher.find(params[:id])
   end
 end
